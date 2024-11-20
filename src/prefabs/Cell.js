6e54a6ab4e2a6efbe67d4756770abb4dd121a392
tblagarrett <tblagarrett@gmail.com>
2024-11-20 12:04:30 -0800

    Prettier formatting changes

--- src/prefabs/Cell.js
+++ src/prefabs/Cell.js
@@ -1,45 +1,45 @@
 class Cell extends Phaser.GameObjects.Container {
-    constructor(scene, x, y) {
-        super(scene, x, y);
-        this.plant = null;
-        this.waterStored = 0;
-    }
+  constructor(scene, x, y) {
+    super(scene, x, y);
+    this.plant = null;
+    this.waterStored = 0;
+  }
 
-    sow(plant) {
-        this.plant = plant;
-    }
+  sow(plant) {
+    this.plant = plant;
+  }
 
-    reap() {
-        plant = this.plant;
-        this.plant = null;
-        return plant;
-    }
+  reap() {
+    plant = this.plant;
+    this.plant = null;
+    return plant;
+  }
 
-    canSow() {
-        if (this.plant != null) {
-            return true;
-        }
-        return false;
+  canSow() {
+    if (this.plant != null) {
+      return true;
     }
+    return false;
+  }
 
-    canReap() {
-        return this.plant.isMaxLevel();
-    }
+  canReap() {
+    return this.plant.isMaxLevel();
+  }
 
-    addWater(amount) {
-        this.waterStored += amount;
+  addWater(amount) {
+    this.waterStored += amount;
 
-        if(this.waterStored > 0) {
-            if(this.plant.needsWater(amount)) {
-            this.plant.addWater(1);
-            this.waterStored--;
-            }
-        }
+    if (this.waterStored > 0) {
+      if (this.plant.needsWater(amount)) {
+        this.plant.addWater(1);
+        this.waterStored--;
+      }
     }
-    
-    addSun() {
-        if(this.plant.needsSun()) {
-            this.plant.addSun();
-        }
+  }
+
+  addSun() {
+    if (this.plant.needsSun()) {
+      this.plant.addSun();
     }
+  }
 }
--- src/prefabs/Grid.js
+++ src/prefabs/Grid.js
@@ -1,40 +1,38 @@
 class Grid extends Phaser.GameObjects.Container {
-    constructor(scene, width, height) {
-        super(scene, width, height, cellSize);
+  constructor(scene, width, height, cellSize) {
+    super(scene);
+    this.scene = scene;
 
-        this.chanceToGen = 0.10;
+    this.chanceToGen = 0.1;
 
-        this.cells = [];
-    
-        for(let i = 0; i < width; i ++) {
-            let row = [];
-            for(let j = 0; j < height; j ++) {
-                row.push(new Cell(scene, i, j));
-            }
-            this.cells.push(row);
+    this.cells = [];
+
+    for (let i = 0; i < width; i++) {
+      let row = [];
+      for (let j = 0; j < height; j++) {
+        row.push(new Cell(scene, i, j));
+      }
+      this.cells.push(row);
+    }
+  }
+
+  timeStep() {
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        let random = Math.random();
+        let cell = this.cells[i][j];
+        if (random < this.chanceToGen / 2) {
+          cell.addSun();
+        } else if (random < this.chanceToGen) {
+          cell.addWater(Math.random(Range(1, 5)));
         }
+
+        // check parameters for plants
+      }
     }
+  }
 
-    timeStep() {
-        for(let i = 0; i < this.width; i ++) {
-            for(let j = 0; j < this.height; j ++) {
-                let random = Math.random();
-                let cell = this.cells[i][j];
-                if(random < this.chanceToGen / 2) {
-                    cell.addSun();
-                } else if (random < this.chanceToGen) {
-                    cell.addWater(Math.random(Range(1,5)));
-                }
-
-                // check parameters for plants
-            }
-        }
-    }
-
-    
-
-    getCell(x,y) {
-        return this.cells[y][x];
-    }
-
+  getCell(x, y) {
+    return this.cells[y][x];
+  }
 }
