1ad3cd0303429eca1a803bfa8907e6b44ca911be
tblagarrett <122318726+tblagarrett@users.noreply.github.com>
2024-11-19 21:03:17 -0800

    Create more base information in the scene file

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -7,9 +7,30 @@ export class Game extends Scene {
 
   create() {
     this.cameras.main.setBackgroundColor(0xffffff);
+
+    this.gridSizeX = 8;
+    this.gridSizeY = 8;
+
+    this.player = null;
+    this.grid = null;
+
+    this.plantsReaped = 0;
   }
 
-  timeStep() {}
+  timeStep() {
+    this.grid.timeStep();
+  }
 
-  sowOrReap() {}
+  sowOrReap(x, y) {
+    let cell = this.grid.getCell(x, y);
+
+    let sown = cell.sow();
+    if (!sown) {
+      if (cell.reap()) {
+        this.plantsReaped++;
+      } // Returns the plant reaped
+    }
+  }
+
+  checkForComplete() {}
 }
