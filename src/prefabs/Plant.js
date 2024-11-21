6f17b91f3d4fb4a45622ccb8fca6c85175189d12
kvanarsd <kvanarsd@ucsc.edu>
2024-11-21 11:48:10 -0800

    Tweaking plant class

--- src/prefabs/Plant.js
+++ src/prefabs/Plant.js
@@ -1,26 +1,21 @@
 import Phaser from "phaser";
 export class Plant extends Phaser.GameObjects.Sprite {
-  constructor(scene, x, y) {
+  constructor(scene, x, y, type) {
     super(scene, x, y);
     this.plant = null;
     this.waterStored = 0;
     this.sunStored = 0;
     this.level = 1;
-    this.maxLevel = 0;
-    this.maxSun = 0;
-    this.maxWater = 0;
-  }
 
-  setPlantType(num) {
-    if (num == 1) {
+    if (type == 1) {
       this.maxLevel = 3;
       this.maxSun = 5;
       this.maxWater = 8;
-    } else if (num == 2) {
+    } else if (type == 2) {
       this.maxLevel = 5;
       this.maxSun = 3;
       this.maxWater = 5;
-    } else if (num == 3) {
+    } else if (type == 3) {
       this.maxLevel = 2;
       this.maxSun = 8;
       this.maxWater = 10;
@@ -28,11 +23,11 @@ export class Plant extends Phaser.GameObjects.Sprite {
   }
 
   needWater() {
-    return typeWaterMax - this.waterStored >= 1;
+    return this.maxWater - this.waterStored >= 1;
   }
 
   needSun() {
-    return typeSunMax - this.sunStored >= 1;
+    return this.maxSun - this.sunStored >= 1;
   }
 
   addWater() {
