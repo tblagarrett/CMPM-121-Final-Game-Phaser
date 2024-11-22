335d116469a432457ad69088143096bf5527e899
ljcui1 <yangyangjiaocui@gmail.com>
2024-11-21 19:07:12 -0800

    adjusting some plant stuff

--- src/prefabs/Plant.js
+++ src/prefabs/Plant.js
@@ -1,32 +1,26 @@
-class Plant extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y) {
-        super(scene, x, y);
-        this.plant = null;
-        this.waterStored = 0;
-        this.sunStored = 0;
-        this.level = 1;
-        this.maxLevel = 0;
-        this.maxSun = 0;
-        this.maxWater = 0;
-    }
+import Phaser from "phaser";
+export class Plant extends Phaser.GameObjects.Sprite {
+  constructor(scene, x, y, type) {
+    super(scene, x, y, this.type);
+    this.plant = null;
+    this.waterStored = 0;
+    this.sunStored = 0;
+    this.level = 1;
 
-    setPlantType(num){
-        if (num == 1){
-            this.maxLevel = 3;
-            this.maxSun = 5;
-            this.maxWater = 8;
-        }
-        else if (num == 2) {
-            this.maxLevel = 5;
-            this.maxSun = 3;
-            this.maxWater = 5;
-        }
-        else if (num == 3) {
-            this.maxLevel = 2;
-            this.maxSun = 8;
-            this.maxWater = 10;
-        }
+    if (this.type == 1) {
+      this.maxLevel = 3;
+      this.maxSun = 5;
+      this.maxWater = 8;
+    } else if (this.type == 2) {
+      this.maxLevel = 4;
+      this.maxSun = 3;
+      this.maxWater = 5;
+    } else if (this.type == 3) {
+      this.maxLevel = 2;
+      this.maxSun = 8;
+      this.maxWater = 10;
     }
+  }
 
     needWater(){
         return typeWaterMax - this.waterStored >= 1;
