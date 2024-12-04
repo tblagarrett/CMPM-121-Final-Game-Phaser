6fa3bdbc2085ea973e56cb3f9ffc78df4fe12637
Sunny Han <shan66@ucsc.edu>
2024-12-03 21:41:12 -0800

    replacing save.png

--- src/prefabs/InternalDSL.ts
+++ src/prefabs/InternalDSL.ts
@@ -1,43 +1,43 @@
-import Phaser from "phaser";
-import { Plant } from "./Plant";
-import { Cell } from "./Cell";
-import Grid from "./Grid";
-import { Game } from "../scenes/Game"; 
-
 export interface PlantConfig {
-    num: number;
-    water: number; 
-    sun: number; 
-    neighbors: number;
-    level: number;
+  num: number;
+  water: number;
+  sun: number;
+  neighbors: number;
+  level: number;
 }
 
 export class InternalDSL {
-    private plants: PlantConfig[];
+  private plants: PlantConfig[];
 
-    static create() {
-        return new InternalDSL();
-    }
+  static create() {
+    return new InternalDSL();
+  }
 
-    definePlantType(num: number, water: number, sun: number, neighbors: number, level: number): this {
-        const config = {num, water, sun, neighbors, level};
-        this.plants.push(config);
-        return this;
-    }
+  definePlantType(
+    num: number,
+    water: number,
+    sun: number,
+    neighbors: number,
+    level: number
+  ): this {
+    const config = { num, water, sun, neighbors, level };
+    this.plants.push(config);
+    return this;
+  }
 
-    getPlantType(type: number) {
-        return this.plants[type];
-    }
+  getPlantType(type: number) {
+    return this.plants[type];
+  }
 
-    getRandPlantType() {
-        return this.plants[getRandomInt(this.plants.length)]
-    }
-    
-    removePlantType(type: number) {
-        return this.plants.splice(1, type);
-    }
-} 
+  getRandPlantType() {
+    return this.plants[getRandomInt(this.plants.length)];
+  }
+
+  removePlantType(type: number) {
+    return this.plants.splice(1, type);
+  }
+}
 
 function getRandomInt(max: number): number {
-    return Math.floor(Math.random() * max);
-  }
+  return Math.floor(Math.random() * max);
+}
