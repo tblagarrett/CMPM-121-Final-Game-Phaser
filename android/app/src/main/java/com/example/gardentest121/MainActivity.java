4e7ad30a75f5cd4dc2634d7fb2d7e32912100a80
ljcui1 <yangyangjiaocui@gmail.com>
2024-12-06 11:40:01 -0800

    testing web app

---
+++ android/app/src/main/java/com/example/gardentest121/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.gardentest121;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.gardentest121',
+  appName: 'Garden Game',
+  webDir: 'dist'
+};
+
+export default config;
